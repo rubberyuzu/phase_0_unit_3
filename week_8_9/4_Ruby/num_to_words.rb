739aca1735541a0596db2352986ef1b2973a9a74
rubberyuzu <yuzu0125@gmail.com>
2014-05-12 22:47:22 -0700

    added blogs

--- week_8_9/4_Ruby/num_to_words.rb
+++ week_8_9/4_Ruby/num_to_words.rb
@@ -1,13 +1,92 @@
 # U3.W8-9: Numbers to English Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
+# define a method called "in_words" which takes one variable (number)
 
 
 
 # 3. Initial Solution
+
+def one_digit(number)
+    if number == 1
+      "one"
+    elsif number == 2
+      "two"
+    elsif number == 3
+      "three"
+    elsif number == 4
+      "four"
+    elsif number == 5
+      "five"
+    elsif number == 6
+      "six"
+    elsif number == 7
+      "seven"
+    elsif number == 8
+      "eight"
+    elsif number == 9
+      "nine"
+    else
+      ""
+    end
+end
+
+def in_words(number)
+  if number.to_s.length == 1
+    one_digit(number)
+  else 
+    if number.to_s.split(//).first == "1"
+      if number ==11
+        "eleven"
+      elsif number ==12
+          "twelve"
+      elsif number ==13
+        "thirteen"
+      elsif number ==14
+        "fourteen"
+      elsif number ==15
+        "fifteen"
+      elsif number ==16
+        "sixteen"
+      elsif number ==17
+        "seventeen"
+      elsif number ==18
+        "eighteen"
+      else 
+        "nineteen"
+      end
+    elsif number.to_s.split(//).first == "2"
+      "twenty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "3"
+      "thirty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "4"
+      "forty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "5"
+      "fifty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "6"
+      "sixty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "7"
+      "seventy"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "8"
+      "eighty"+one_digit(number.to_s.split(//).last.to_i)
+    else
+      "ninty"+one_digit(number.to_s.split(//).last.to_i)
+    end
+  end
+end
+
+        
+        
+        
+        
+        
+        
+        
+        
+    
 
 
 
@@ -15,6 +94,73 @@
 # 4. Refactored Solution
 
 
+def one_digit(number)
+    if number == 1
+      "one"
+    elsif number == 2
+      "two"
+    elsif number == 3
+      "three"
+    elsif number == 4
+      "four"
+    elsif number == 5
+      "five"
+    elsif number == 6
+      "six"
+    elsif number == 7
+      "seven"
+    elsif number == 8
+      "eight"
+    elsif number == 9
+      "nine"
+    else
+      ""
+    end
+end
+
+def in_words(number)
+  if number.to_s.length == 1
+    one_digit(number)
+  else 
+    if number.to_s.split(//).first == "1"
+      if number ==11
+        "eleven"
+      elsif number ==12
+          "twelve"
+      elsif number ==13
+        "thirteen"
+      elsif number ==14
+        "fourteen"
+      elsif number ==15
+        "fifteen"
+      elsif number ==16
+        "sixteen"
+      elsif number ==17
+        "seventeen"
+      elsif number ==18
+        "eighteen"
+      else 
+        "nineteen"
+      end
+    elsif number.to_s.split(//).first == "2"
+      "twenty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "3"
+      "thirty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "4"
+      "forty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "5"
+      "fifty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "6"
+      "sixty"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "7"
+      "seventy"+one_digit(number.to_s.split(//).last.to_i)
+    elsif number.to_s.split(//).first == "8"
+      "eighty"+one_digit(number.to_s.split(//).last.to_i)
+    else
+      "ninty"+one_digit(number.to_s.split(//).last.to_i)
+    end
+  end
+end
 
 
 
